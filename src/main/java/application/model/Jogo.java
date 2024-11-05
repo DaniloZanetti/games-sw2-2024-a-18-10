f1657010119ff172b4f34080048171dfe9a8b6e0
DaniloZanetti <danilozanetti123@gmail.com>
2024-11-05 13:33:44 +0000

    aula0511

--- src/main/java/application/model/Jogo.java
+++ src/main/java/application/model/Jogo.java
@@ -1,5 +1,10 @@
 package application.model;
+ 
+import java.util.Set;
 
+import org.eclipse.tags.shaded.org.apache.bcel.generic.PUSH;
+
+import application.record.JogoDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,19 +14,31 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+ 
 @Entity
 @Table(name = "jogos")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Jogo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(nullable = false)
     private String titulo;
-
     @ManyToMany
+ 
     @JoinTable(name = "jogos_possuem_plataformas",
-       joinColumns = @JoinColumn(name = "id_jogo"),
-       inverseJoinColumns = @JoinColumn(name = "id_plataforma"))
+        joinColumns = @JoinColumn(name = "id_jogo"),
+        inverseJoinColumns = @JoinColumn(name = "id_plataforma"))
     private Set<Plataforma> plataformas;
+
+    public Jogo(JogoDTO jogo) {
+        this.id = jogo.id();
+        this.titulo = jogo.titulo();
+        this.plataformas = jogo.plataformas();
+    }
 }
